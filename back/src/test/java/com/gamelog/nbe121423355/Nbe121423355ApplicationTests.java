82bfd4773147a26c9845c6161aac9c1af68dc7cc
ansckddnjs17 <ansckddnjs170@naver.com>
2026-09-14 15:39:03 +0900

    CI 설정

--- back/src/test/java/com/gamelog/nbe121423355/Nbe121423355ApplicationTests.java
+++ back/src/test/java/com/gamelog/nbe121423355/Nbe121423355ApplicationTests.java
@@ -2,8 +2,10 @@ package com.gamelog.nbe121423355;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Nbe121423355ApplicationTests {
 
     @Test
