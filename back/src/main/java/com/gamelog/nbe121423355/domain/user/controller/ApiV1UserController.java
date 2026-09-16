f513893460416c8b8cd0a35fae030d63bb4de4e8
yosi475 <yosi475@naver.com>
2026-09-16 14:10:27 +0900

    feat: 내 정보 조회 및 온보딩 완료/건너뛰기 api추가

--- back/src/main/java/com/gamelog/nbe121423355/domain/user/controller/ApiV1UserController.java
+++ back/src/main/java/com/gamelog/nbe121423355/domain/user/controller/ApiV1UserController.java
@@ -3,11 +3,13 @@ package com.gamelog.nbe121423355.domain.user.controller;
 import com.gamelog.nbe121423355.domain.user.dto.*;
 import com.gamelog.nbe121423355.domain.user.service.UserService;
 import com.gamelog.nbe121423355.global.dto.RsData;
+import com.gamelog.nbe121423355.global.security.SecurityUser;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.Duration;
@@ -65,6 +67,43 @@ public class ApiV1UserController {
                 "200-2",
                 "토큰 재발급 성공",
                 tokenResponseDto
+        );
+    }
+
+    @GetMapping("/me")
+    public RsData<UserDto> getMe(
+            @AuthenticationPrincipal SecurityUser securityUser
+    ){
+        UserDto userDto = userService.getMe(securityUser.getId());
+        return new RsData<>(
+                "200-3",
+                "내 정보 조회 성공",
+                userDto
+        );
+    }
+
+    @PatchMapping("/me/onboarding")
+    public RsData<UserDto> onboarding(
+        @AuthenticationPrincipal SecurityUser securityUser
+    ){
+        UserDto userDto = userService.exitOnboarding(securityUser.getId());
+        return new RsData<>(
+                "200-4",
+                "온보딩 완료",
+                userDto
+        );
+    }
+
+
+    @PatchMapping("/me/onboarding/skip")
+    public RsData<UserDto> onboardingSkip(
+            @AuthenticationPrincipal SecurityUser securityUser
+    ){
+        UserDto userDto = userService.exitOnboarding(securityUser.getId());
+        return new RsData<>(
+                "200-5",
+                "온보딩 스킵",
+                userDto
         );
     }
 }
